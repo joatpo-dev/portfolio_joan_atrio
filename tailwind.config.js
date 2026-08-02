5c4e6e607397b1f95fc23589a37eebfaba9241b9
Joan Atrio <joan@ecodeliver.tech>
2026-08-02 18:39:01 +0200

    chore: Makes use of pnpm and vite, removes unnecesary parts of the repo and updates git ignore

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,html}", "./public/index.html"],
+  content: ["./index.html", "./src/**/*.{js,jsx,html}"],
   theme: {
     extend: {
       colors: {
